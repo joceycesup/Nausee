47d6fc0b4d681ca5601cba299a6b6d0435aac1a6
thomasfrancisco <thofrancisco@gmail.com>
2016-11-12 14:19:00 +0100

    TalkieWalkie detecte les sons

--- Assets/Scripts/WalkieTalkie.cs
+++ Assets/Scripts/WalkieTalkie.cs
@@ -3,93 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class WalkieTalkie : MonoBehaviour {
-	private AudioSource microphone;
-	private int sampleDataLength = 2048;
-	private float updateStep = 0.1f; // delai de mise a jour des valeurs
-	private float currentUpdateTime = 0.0f;
-	private float clipLoudness; // volume moyen enregistre par le micro pendant un laps de temps donne (updateStep)
-	private float loudnessThreshold; // seuil au-dela duquel on considere que le joueur parle
-	private float[] clipSampleData; // tampon pour recuperer les donnees sonores du micro
+    private micInput micro;
+    private float lastUpdateTime;
+    private float currentTime;
 
-	private float maxLoudness = 0;
-
-	private List<float> loudnessValues; // utilise pour stocker les valeurs en cours de calibration du niveau moyen de la voix
-	private float averageLoudness; // niveau moyen de la voix
-
+	public float loudnessThreshold; // sensibilité du micro
+    public float soundTimeBeforeDetect; //Vérifie qu'il y a un son toutes les x secondes;
+    
 	// Use this for initialization
 	void Start () {
-		loudnessValues = new List<float> ();
-
-		//voiceOver = gameObject.GetComponent<AudioSource> (); // ne fais pas gaffe a ca, sera utilise plus tard
-		microphone = gameObject.AddComponent<AudioSource> (); // le micro doit etre associe a une audiosource pour pouvoir enregistrer
-		clipSampleData = new float[sampleDataLength];
-		StartMicrophone ();
-	}
-
-	void OnApplicationFocus (bool hasFocus) { // pour éviter d'avoir un délai sur le micro en arretant l'execution ou en perdant le focus sur unity
-		if (microphone != null) {
-			if (hasFocus) {
-				StartMicrophone ();
-			} else {
-				microphone.Pause ();
-			}
-		}
-	}
-
-	void StartMicrophone () {
-		microphone.clip = Microphone.Start (null, true, 10, 44100);
-		microphone.loop = true;
-		//microphone.mute = true;
-		microphone.volume = 0;
-		while (!(Microphone.GetPosition (null) > 0)) {}
-		microphone.Play ();
+        micro = gameObject.GetComponent<micInput>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		currentUpdateTime += Time.deltaTime;
-		if (currentUpdateTime >= updateStep) { // tous les updateStep, on actualise le volume moyen
-			currentUpdateTime = 0.0f;
-			microphone.clip.GetData (clipSampleData, microphone.timeSamples);
-			clipLoudness = 0.0f;
-			foreach (var sample in clipSampleData) {
-				clipLoudness += Mathf.Abs (sample);
-			}
-			clipLoudness /= sampleDataLength;
+        if (currentTime > soundTimeBeforeDetect)
+        {
+            currentTime = 0;
+            if (isTalking())
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f);
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+            }
+            lastUpdateTime = Time.time;
+        } else
+        {
+            currentTime = Time.time - lastUpdateTime;
+        }
 
-			// on affiche le clipLoudness courant en appuyant sur cette touche, evite d'etre envahi par les Debug
-			if (Input.GetButton("Fire2"))
-				Debug.Log ("Loudness : " + clipLoudness);
-			if (clipLoudness > maxLoudness) {
-				maxLoudness = clipLoudness;
-			}
 
-			//gameObject.transform.Translate (new Vector3 (0, Time.deltaTime * clipLoudness * 5.0f, 0));
-			Color c = new Color(1, IsTalking()?0:1, IsTalking()?0:1);
-			gameObject.GetComponent<SpriteRenderer> ().color = c;
+    }
 
-			if (Input.GetButton ("Fire1")) { // on commence la calibration du micro en appuyant sur ce bouton
-				loudnessValues.Add (clipLoudness);
-			}
-		}
-		if (Input.GetButtonUp ("Fire1")) { // on arrete la calibration du micro et on affecte la moyenne puis le seuil
-			averageLoudness = 0.0f;
-			foreach (float lv in loudnessValues) {
-				averageLoudness += lv;
-			}
-			if (loudnessValues.Count > 0) { // evite d'avoir un NaN
-				averageLoudness /= loudnessValues.Count;
-			}
-			loudnessValues.Clear ();
 
-			// 2.0f -> valeur arbitraire, on met le seuil a la moitie de la moyenne du volume lors de la calibration
-			// change cette valeur comme tu veux ou utilise une autre technique pour definir le seuil, c'est toi qui vois
-			loudnessThreshold = averageLoudness / 2.0f;
-			Debug.Log ("Average : " + averageLoudness);
-		}
-	}
-
-	public bool IsTalking () {
-		return clipLoudness > loudnessThreshold;
+    public bool isTalking () {
+		return micro.volume > loudnessThreshold;
 	}
 }
---
+++ Assets/Scripts/micInput.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class micInput : MonoBehaviour {
+
+    public float volume;
+    public static float MicLoudness;
+    private string _device;
+    private AudioClip _clipRecord = new AudioClip();
+    private int _sampleWindow = 128;
+    private bool _isInitialized;
+
+    void InitMic()
+    {
+        if (_device == null)
+        {
+            _device = Microphone.devices[0];
+            _clipRecord = Microphone.Start(_device, true, 999, 44100);
+            Debug.Log(_clipRecord);
+        }
+    }
+
+    void StopMicrophone()
+    {
+        Microphone.End(_device);
+    }
+
+    float LevelMax()
+    {
+        float levelMax = 0;
+        float[] waveData = new float[_sampleWindow];
+        int micPosition = Microphone.GetPosition(null) - (_sampleWindow + 1);
+        if (micPosition < 0)
+        {
+            return 0;
+        }
+        _clipRecord.GetData(waveData, micPosition);
+        for (int i = 0; i < _sampleWindow; ++i)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        return levelMax;
+    }
+
+    void Update()
+    {
+        MicLoudness = LevelMax();
+        volume = MicLoudness;
+
+    }
+
+    void OnEnable()
+    {
+        InitMic();
+        _isInitialized = true;
+    }
+
+    void OnDisable()
+    {
+        StopMicrophone();
+    }
+
+    void OnDestory()
+    {
+        StopMicrophone();
+    }
+
+    void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            if (!_isInitialized)
+            {
+                InitMic();
+                _isInitialized = true;
+            }
+        }
+
+        if (!focus)
+        {
+            StopMicrophone();
+            _isInitialized = false;
+        }
+    }
+}
